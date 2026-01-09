8b82e05ce9ad9ce09496f5998e23a2d34e08c9b8
Pufikas <pufikas@proton.me>
2026-01-09 15:46:12 +0200

    tweak options

--- deploy.js
+++ deploy.js
@@ -1,16 +1,18 @@
 const FormData = require("form-data");
 let url = "https://nekoweb.org/api/files/upload";
 const form = new FormData();
-
-form.append("pathname", "/test");
-form.append("files", "test.txt", { filepath: "./test.txt" });
-
 let { APIKEY, DOMAIN, USERNAME, DIRECTORY } = process.env;
+
+const srcDir = path.resolve("..", DIRECTORY);
+
+form.append("pathname", `/${DIRECTORY}`);
+form.append("files", filename, { filepath: srcDir });
 
 let options = {
     method: "POST",
     headers: {
-        Authorization: APIKEY
+        Authorization: APIKEY,
+        ...form.getHeaders()
     }
 };
 
